b9d14bfba64b4240950c666076e606f1755ee6fe
Facufr12 <facufr2@hotmail.com>
2024-03-22 23:54:47 -0300

    pasaje a produ

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -4,7 +4,7 @@ export function Character(character) {
   return (
     <div className="character-card">
       <h5 className="character-card-title">{character.name}</h5>
-      <img className="character-card-image img-fluid rounded-pill " src={character.image} alt={character.name} />
+      <img className="character-card-image img-fluid rounded-pill" src={character.image} alt={character.name} />
       <div className="character-card-body">
         <p>Status: {character.status}</p>
       </div>
